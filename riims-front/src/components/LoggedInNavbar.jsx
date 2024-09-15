3f5b2c3139e58cd7c20ec132d78a5baacdabbcfa
adeaberisha <ab67623@ubt-uni.net>
2024-09-16 01:00:42 +0200

    Slight design changes to navbar and home page
    Removed the dropdown from navbar, changed some fonts,texts and colors.

--- riims-front/src/components/LoggedInNavbar.jsx
+++ riims-front/src/components/LoggedInNavbar.jsx
@@ -27,38 +27,22 @@ function LoggedInNavbar({ handleLogout }) {
     <Navbar expand="lg" className="custom-navbar bg-light text-light shadow-sm">
       <Container>
         <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
-          <img src={RiimsLogo} className='logo mr-5' alt="Riims Logo" />
+          <img src={RiimsLogo} className="logo mr-5" alt="Riims Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavDropdown title="Menu" id="basic-nav-dropdown" className="text-light">
-              <NavDropdown.Item as={Link} to="/thecv">CV</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/edit-profile">Edit Profile</NavDropdown.Item>
-              {/* <NavDropdown.Item as={Link} to="/eksperienca">Eksperienca</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/aftesite">Aftesite</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/specializimet">Specializimet</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/gjuhet">Gjuhet</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/licensat">Licensat</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/projekti">Projekti</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/puna-vullnetare">Puna Vullnetare</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/edukimi">Edukimi</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/honorsandawards">Honors And Awards</NavDropdown.Item>
-             <NavDropdown.Item as={Link} to="/EditLicensa">EditLicensa</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/EditEksperienca">EditEksperienca</NavDropdown.Item> */}
-              <NavDropdown.Item as={Link} to="/personDetails">Person Details</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link as={Link} to="/Home" className="text-light">Home</Nav.Link>
-            
-            
+            <Nav.Link as={Link} to="/edit-profile">Edit Profile</Nav.Link>
+            <Nav.Link as={Link} to="/personDetails">CV Details</Nav.Link>
+            <Nav.Link as={Link} to="/Home">Home</Nav.Link>
           </Nav>
           {isAdmin() && (
             <Nav>
-              <Nav.Link as={Link} to="/Dashboard" className="text-light">Dashboard</Nav.Link>
+              <Nav.Link as={Link} to="/Dashboard">Dashboard</Nav.Link>
             </Nav>
           )}
           <Nav>
-            <Nav.Link onClick={handleLogout} className="text-light">Logout</Nav.Link>
+            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
